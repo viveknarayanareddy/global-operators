9d1f1c1f2c25a62941818401b7498ffcb8662b7c
viveknarayanareddy <kviveknarayanareddy@gmail.com>
2022-08-18 09:01:06 +0530

    global variables

---
+++ global.js
@@ -0,0 +1,7 @@
+let day = "Monday";
+let color = "Green";
+if(day=="Monday")
+{
+    let color = "Blue";
+    console.log("it is monday.The sky is of" +color);
+}
---
+++ local.js
@@ -0,0 +1,4 @@
+var fullname="KATHI CHANDU";
+
+console.log("My name is:")
+console.log(fullname);
